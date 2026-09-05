d265f5d8385b5ba5063986cee7d2c97e3a9401b5
Vjaceslavs Gabrans <zlavik228@gmail.com>
2026-09-05 22:31:37 +0300

    Update 9.05.2026

--- AI/AI.py
+++ AI/AI.py
@@ -9,5 +9,3 @@ response = client.responses.create(
 )
 
 print(response.output_text)
-
-
---
+++ AI/helper.py
@@ -0,0 +1,16 @@
+my_name = "Vjaceslavs"
+my_last_name = "Gabrans"
+
+stock = my_name , my_last_name
+print(stock)
+
+#-------------------------------
+apple = 3
+banana = 4 
+stock = apple + banana
+if apple == banana:
+    print("Good")
+elif stock == 7:
+    print("No space")
+else:
+    print("Error")
